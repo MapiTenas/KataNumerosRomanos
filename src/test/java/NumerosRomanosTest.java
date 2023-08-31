4a9d9dd3adda072c41f978e80d1135cfacd6690b
Mapi <mapitenar@hotmail.com>
2023-08-31 17:33:17 +0200

    Test 2 KO

--- src/test/java/NumerosRomanosTest.java
+++ src/test/java/NumerosRomanosTest.java
@@ -4,8 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NumerosRomanosTest {
     @Test
-    void numeroRomanosConversor() {
+    void numeroRomanosConversor1() {
 
         assertEquals("I", new NumerosRomanos().convertir(1));
+    }
+
+    @Test
+    void numeroRomanosConversor2() {
+
+        assertEquals("II", new NumerosRomanos().convertir(2));
     }
 }
